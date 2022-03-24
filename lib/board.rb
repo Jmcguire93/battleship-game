8520163a9e093afb78bb8e2af3c849961e43ca81
James <james.ryan.mcguire@gmail.com>
2022-03-24 15:37:16 -0400

    Add [] method to represent grid position

--- lib/board.rb
+++ lib/board.rb
@@ -1,8 +1,13 @@
 class Board
   attr_reader :size 
-  
+
   def initialize(n)
     @grid = Array.new(n) { Array.new(n, :N) }
     @size = n * n 
   end 
+
+  def [](position)
+    row, column = position 
+    @grid[row][column]
+  end
 end
