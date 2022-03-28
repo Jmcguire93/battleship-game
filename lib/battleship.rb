861e87a2841027bfebeeeeb870ceeaf7c0f21b09
James <james.ryan.mcguire@gmail.com>
2022-03-28 13:59:05 -0400

    Add start_game method

--- lib/battleship.rb
+++ lib/battleship.rb
@@ -9,4 +9,10 @@ class Battleship
     @board = Board.new(n)
     @remaining_misses = @board.size * 0.5
   end
+
+  def start_game 
+    @board.place_random_ships
+    puts "Number of ships: #{@board.num_ships}"
+    @board.print 
+  end
 end
